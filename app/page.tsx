73c910573a9f58d81f251ff900dc76bea3288ff9
Komenpurity <jepkorirpurity8@gmail.com>
2023-08-17 15:15:17 +0300

    added page responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -20,17 +20,18 @@ export default async function Home({searchParams}: HomeProps) {
      <h1 className='text-3xl font-extrabold'>Harry Potter Characters</h1>
      <SearchBar />
 
-          <div className='grid grid-cols-5 m-2'> 
+          <div className='grid grid-cols-5 m-2 md:grid lg:grid-cols-4 gap-[30px] max-md:grid-cols-2 max-sm:flex max-sm:flex-col max-sm:items-center max-sm:justify-center'> 
             {searchHouses?.map((element: CharacterProps) => {
               //iterating over the search results from the API
               return <SearchDisplay element={element} />
             })}
           </div>
+        
 
       {!isDataEmpty ? (
         <div>
           <h1 className='text-1xl text-purple-800 text-lg p-2 font-bold text-center'>Harry Potter Details</h1>
-          <div className='grid grid-cols-5 m-2'>   
+          <div className='grid grid-cols-5 m-2 md:grid lg:grid-cols-4 gap-[30px] max-md:grid-cols-2 max-sm:flex max-sm:flex-col max-sm:items-center max-sm:justify-center'>   
             {allCharacters?.map((char) => {
               //assigning the data to the char 
               return <CharacterCard char={char}/>  
